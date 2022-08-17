e466204c565db70add14464dc231e988c357bb52
Osumah Favour <osumahft@gmail.com>
2022-08-17 14:29:57 -0700

    add

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,42 +6,42 @@ size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
+ *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ *         Otherwise - the number of unique nodes in the list.
  */
 
 size_t looped_listint_len(const listint_t *head)
 
 {
-const listint_t *slowP, *fastP;
+const listint_t *tortoise, *hare;
 size_t nodes = 1;
 if (head == NULL || head->next == NULL)
 return (0);
-slowP = head->next;
-fastP = (head->next)->next;
-while (fastP)
+tortoise = head->next;
+hare = (head->next)->next;
+while (hare)
 {
-if (slowP == fastP)
+if (tortoise == hare)
 {
-slowP = head;
-while (slowP != fastP)
+tortoise = head;
+while (tortoise != hare)
 {
 nodes++;
-slowP = slowP->next;
-fastP = fastP->next;
+tortoise = tortoise->next;
+hare = hare->next;
 }
-slowP = slowP->next;
-while (slowP != fastP)
+tortoise = tortoise->next;
+while (tortoise != hare)
 {
 nodes++;
-slowP = slowP->next;
+tortoise = tortoise->next;
 }
 return (nodes);
 }
-slop = slowP->next;
-fastP = (fastP->next)->next;
+tortoise = tortoise->next;
+hare = (hare->next)->next;
 }
 return (0);
 }
